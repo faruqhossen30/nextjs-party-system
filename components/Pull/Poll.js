20b523d910c2704c56a9350925cd0502be6ac305
faruqhossen30 <faruqhossen30@gmail.com>
2022-11-20 17:27:42 +0600

    user can now poll

--- components/Pull/Poll.js
+++ components/Pull/Poll.js
@@ -8,9 +8,26 @@ const Poll = ({ user }) => {
     const [isLoading, setIsLoading] = useState(true)
     const [polls, setPolls] = useState([])
 
-    const pollAttendanceSubmitHandaller = (val1,val2)=>{
+    const pollAttendanceSubmitHandaller = (val1, val2) => {
         console.log('val1', val1)
         console.log('val2', val2);
+    }
+    const changePoll = (e) => {
+        console.log('poll_option_id', e.target.value);
+        console.log('poll_id', e.target.getAttribute('data-poll'));
+        let poll_id = e.target.getAttribute('data-poll');
+        let poll_option_id = e.target.value;
+
+        axios.post(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/user/poll/option/attendance/${poll_id}`,{
+            poll_option_id: poll_option_id
+        })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log('this is error', err);
+                setError(err.response.data.message)
+            })
     }
 
     useEffect(() => {
@@ -33,12 +50,12 @@ const Poll = ({ user }) => {
                                 <FaPoll />
                                 <span> {poll.title}</span>
                             </h6>
-                            <form onSubmit={()=>pollAttendanceSubmitHandaller(poll.id)}>
-                                <input type="hidden" value={poll.id}  />
+                            <form onSubmit={() => pollAttendanceSubmitHandaller(poll.id)}>
+                                <input type="hidden" value={poll.id} />
                                 {poll.options.map((option, index) => {
                                     return (
                                         <label htmlFor={`optionFor${option.id}`} className='flex space-x-2 py-1' key={index}>
-                                            <input type="checkbox" name="poll_option_id" value={option.id} id={`optionFor${option.id}`} />
+                                            <input type="radio" name="poll_option_id" data-poll={poll.id} onChange={changePoll} value={option.id} id={`optionFor${option.id}`} />
                                             <span>{option.option}</span>
                                         </label>
                                     )
